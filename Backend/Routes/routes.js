6a6ddd755c3df58f85b66c2537e248a1553e2ea7
Shravan Kumar Singh <sarvan17@duck.com>
2024-08-10 23:25:08 +0530

    update

--- Backend/Routes/routes.js
+++ Backend/Routes/routes.js
@@ -1,14 +1,13 @@
 const { Router } = require("express");
-const runCode = require("../Controllers/runCode.js");
-const submitCode = require("../Controllers/submitCode.js");
 const problem_detail = require("../Controllers/problem_detail.js");
 const addProblem = require("../Controllers/addproblem.js");
 const problem_list = require("../Controllers/problem_list.js");
+const processCode = require('../Controllers/processCode.js')
 
 const router = Router();
 
-router.post("/run", runCode);
-router.post("/submitCode", submitCode);
+router.post("/run", processCode);
+router.post("/submitCode", processCode);
 router.post("/problem_detail", problem_detail);
 router.post("/addproblem", addProblem);
 router.get("/problem_list", problem_list);
